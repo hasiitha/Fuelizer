f37352ea4072ad22cc231d3355dcb5b9bb06ff5f
Thimal Ushara <thimalushara@gmail.com>
2022-10-18 12:04:45 +0530

    login UI init and navigation init

---
+++ Fuelizer-Frontend/app/src/main/java/com/example/fuelizer/StationOwnerRegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.fuelizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StationOwnerRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_station_owner_register);
+    }
+}
---
+++ Fuelizer-Frontend/app/src/main/java/com/example/fuelizer/VehicalOwnerRegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.fuelizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VehicalOwnerRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehical_owner_register);
+    }
+}
