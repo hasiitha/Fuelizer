0d8011de304d5732ce2d0289f2cf65b60c5e15da
Thimal Ushara <thimalushara@gmail.com>
2022-10-18 22:01:13 +0530

    hasitha init

--- Fuelizer-Frontend/app/src/main/java/com/example/fuelizer/MainActivity.java
+++ Fuelizer-Frontend/app/src/main/java/com/example/fuelizer/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn_v_owner, btn_s_owner;
+    private Button btn_v_owner, btn_s_owner, haitha_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
         btn_v_owner=findViewById(R.id.btn_vehical_owner);
         btn_s_owner = findViewById(R.id.btn_station_owner);
+        haitha_btn = findViewById(R.id.haitha_btn);
 
         btn_v_owner.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +34,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 intent.putExtra("userType", "stationOwner");
+                startActivity(intent);
+            }
+        });
+
+        haitha_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PetrolStationRegistration.class);
+
                 startActivity(intent);
             }
         });
