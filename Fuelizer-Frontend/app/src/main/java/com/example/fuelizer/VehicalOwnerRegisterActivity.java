3b077ffa5dcb3ebf135cb037fac68c623b3f3c97
Thimal Ushara <thimalushara@gmail.com>
2022-10-24 15:01:54 +0530

    user login registration intergrated

--- Fuelizer-Frontend/app/src/main/java/com/example/fuelizer/VehicalOwnerRegisterActivity.java
+++ Fuelizer-Frontend/app/src/main/java/com/example/fuelizer/VehicalOwnerRegisterActivity.java
@@ -7,16 +7,35 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class VehicalOwnerRegisterActivity extends AppCompatActivity {
 
     private TextView txtView_login_registeredUser;
     private EditText editTxt_username, editTxt_password, editTxt_nic, editTxt_mobile, editTxt_vehicleType, editTxt_fuelType, editTxt_vehicleNumber, editText_chasisNumber;
     private Button btn_register;
+//    private ProgressBar vehicleOwnerPB;
     private String userType;
     private DBHelper DB;
+    private RequestQueue requestQueue;
 
 
     @Override
@@ -38,6 +57,7 @@ public class VehicalOwnerRegisterActivity extends AppCompatActivity {
         editTxt_fuelType = findViewById(R.id.editTxt_Regis_fuelType);
         editTxt_vehicleNumber = findViewById(R.id.editTxt_Regis_vehicleNumber);
         editText_chasisNumber = findViewById(R.id.editTxt_Regis_chasisNumber);
+//        vehicleOwnerPB = findViewById(R.id.vehicle_owner_PB);
 
         btn_register = findViewById(R.id.btn_vehicleOwner_register);
 
@@ -55,11 +75,13 @@ public class VehicalOwnerRegisterActivity extends AppCompatActivity {
                 chasisNumber = editText_chasisNumber.getText().toString();
 
                 if(uname.equals("") || pw.equals("") || nic.equals("") || mobile.equals("") || vType.equals("") || fType.equals("") || vNumber.equals("") || chasisNumber.equals("")){
+
                     Toast.makeText(VehicalOwnerRegisterActivity.this, "Enter all the fields !", Toast.LENGTH_SHORT).show();
                 }else {
                     Boolean checkUser = DB.checkusername(uname);
                     if(checkUser == false){
                         Boolean insertSuccess = DB.insertData(uname, pw, userType);
+                        postDataToDB(uname,nic,mobile,vType,fType,vNumber,chasisNumber);
                         if(insertSuccess == true){
                             Toast.makeText(VehicalOwnerRegisterActivity.this, "User Register Successfully !", Toast.LENGTH_SHORT).show();
                             Intent intent = new Intent(VehicalOwnerRegisterActivity.this, LoginActivity.class);
@@ -89,4 +111,42 @@ public class VehicalOwnerRegisterActivity extends AppCompatActivity {
 
 
     }
+
+    private void postDataToDB(String name, String nic, String mobile, String vType, String fType, String vNumber, String chasisNumber ){
+        // url to post the user data
+        String url = "http://192.168.1.11:8081/api/Customer";
+//        vehicleOwnerPB.setVisibility(View.VISIBLE);
+
+        HashMap<String, String> params = new HashMap<String, String>();
+
+        params.put("userName", name);
+        params.put("nic", nic);
+        params.put("mobileNumber", mobile);
+        params.put("vehicleType", vType);
+        params.put("fuelType", fType);
+        params.put("vehicleNumber", vNumber);
+        params.put("chasisNumber", chasisNumber);
+
+        JsonObjectRequest req = new JsonObjectRequest(url, new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            VolleyLog.v("Response:%n %s", response.toString(4));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.e("Error: ", error.getMessage());
+            }
+        });
+
+        requestQueue = Volley.newRequestQueue(VehicalOwnerRegisterActivity.this);
+        requestQueue.add(req);
+
+    }
+
 }
